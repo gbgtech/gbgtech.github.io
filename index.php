98dd95e63d65b24d712f98f5f338d4c624bd39e6
Andreas Rolén <andreasrolen93@gmail.com>
2015-03-06 16:13:48 +0100

    Changed site to new gbgtech design

---
+++ index.php
@@ -0,0 +1,92 @@
+
+<html class="no-js" lang="en">
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>#GBGTECH</title>
+  <link rel="stylesheet" href="css/foundation.css" />
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <script src="js/vendor/modernizr.js"></script>
+<link rel="icon" type="image/ico" href="http://www.andreasrolen.com/gbgtech/img/gbgtech_logo_blue.png"/>
+  <meta name="keywords" content="gbgtech, gbgtechweek, gbg, entrepreneurship, startup, tech, gothenburg, göteborg">
+  <meta name="description" content="#GBGTECH - Gothenburg's Tech and Startup Community">
+  <meta property="og:title" content="#GBGTECH">
+  <meta property="og:description" content="#GBGTECH - Gothenburg's Tech and Startup Community">
+
+</head>
+<body>
+  <header>
+
+
+  </header>
+
+  <div id="main">
+    <div class="row logo-row">
+      <div class="small-12 columns">
+        <div class="logo-container">
+          <img class="logo" src="img/gbgtech_logo_blue.png">
+          <h1 class="logo_headline">Gothenburg's Tech and Startup Community</h1>
+          <a href="https://twitter.com/search?f=realtime&q=%23gbgtech&src=typd" target="_blank"><img class="twitter" src="img/Twitter_logo_black.png" onmouseover="this.src='img/Twitter_logo_gbgblue.png'"
+          onmouseout="this.src='img/Twitter_logo_black.png'"></a>
+        </div>
+      </div>
+    </div>
+    <div class="row definition-row">
+      <div class="small-12 columns">
+        <div class="definition-container">
+          <h1>What is #gbgtech?</h1>
+          <h2>#gbgtech wants to position Gothenburg as an attractive tech- and startup-commuity in Scandinavia.</h2>
+          <h3>The purpose of #gbgtech is two-folded:</h3>
+          <div class="purpose-container">
+            <ol>
+              <li>Adding light to the tech- and startup scene in Gothenburg</li>
+              <ol>
+                <li>Locally, nationally, internationally</li>
+                <li>Draw business and investment attention</li>
+              </ol>
+              <li>Being a portal</li>
+              <ol>
+                <li>Entry gate to participate in the tech- & startup scene in Gothenburg</li>
+              </ol>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row who-row">
+      <div class="small-12 columns">
+        <div class="who-container">
+        <h1>Who is a part of #gbgtech?</h1>
+          <div class="who-list-container">
+            <ol>
+              <li>You who consider yourself to be within
+                <ol>
+                  <li>Tech</li>
+                  <li>Startup</li>
+                  <li>Or Tech- and Startup</li>
+                </ol>
+              </li>
+              <br />
+              <li>You who believe in empowering the tech- and startup-community of Gothenburg</li>
+              <br />
+              <li>You who are curious about the tech- and startup-community of Gothenburg</li>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <footer>
+    <p>New site is under development</p>
+
+  </footer>
+  <script src="js/vendor/jquery.js"></script>
+  <script src="js/foundation.min.js"></script>
+  <script src="js/foundation/topbar.js"></script>
+  <script>
+    $(document).foundation();
+  </script>
+</body>
+</html>
